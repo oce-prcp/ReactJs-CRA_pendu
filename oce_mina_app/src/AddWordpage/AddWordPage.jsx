60a48ffb28e69a77348ab5525edb02188b5fbb8d
minaarafat19 <aminayoussoufarafat@gmail.com>
2022-02-15 15:37:53 +0100

    1 try

---
+++ oce_mina_app/src/AddWordpage/AddWordPage.jsx
@@ -0,0 +1,85 @@
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Button,
+  Form,
+ Col,Container,Row
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { WordProvider } from "./../Provider/WordProvider";
+
+// DE BASE MON TABLEAU EST VIDE ET DONC MES STATES AUSSI ...
+export default function AddWordPage() {
+  const [wordAdd, setWordAdd] = useState({
+    id: "",
+    mots: "",
+   
+  });
+
+  
+  const wordProvider = new WordProvider();
+  const navigate = useNavigate();
+
+  function add(e) {
+    e.preventDefault();
+    wordProvider.add(wordAdd);
+    navigate("/WordPage/:id");
+  }
+
+
+
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Ajouter un Mot</h1>
+            <hr />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6}>
+            <Form onSubmit={e => add(e)}>
+              <Form.Group className="mb-3">
+                <Form.Label>Titre</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter mots"
+                  value={wordAdd.mots}
+                  onChange={e => {
+                    let tmp = { ...wordAdd }
+                    tmp.mots = e.target.value
+                    setWordAdd(tmp)
+                  }}
+                  required
+                />
+              </Form.Group>
+              
+              
+             
+
+              <hr />
+
+              <Button variant="light" as={Link} to="/WordPage/:id">
+                Retour
+              </Button>
+
+              <Button
+                variant="outline-secondary"
+                className="float-end mx-2"
+                type="reset"
+              >
+                Annuler
+              </Button>
+
+              <Button variant="success" type="submit" className="float-end" >
+                Enregistrer
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
